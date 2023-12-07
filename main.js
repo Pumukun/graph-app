ac69e5025c18afc741fe6bb2125ad087e45ede7d
pumukun <demgisha@gmail.com>
2023-12-07 23:17:33 +0300

    made translation from matrix to list alg to contructor

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ class Graph {
      * @type {Array.<Array.<T>>} adjMatrix
      * @type {object} adjList
     */
-    #adjMatrix = [[]]
-    #adjList = {}
-    
+    #adjMatrix
+    #adjList 
+
     /*
      * @constructor
      * @param {Array.<Array.<T>>|object} arg
@@ -18,9 +18,21 @@ class Graph {
     constructor(arg) {
         if (Array.isArray(arg)) {
             this.#adjMatrix = arg;
-
+            this.#adjList = new Object();
+             
+            for (var i = 0; i < this.#adjMatrix.length; i++) {
+                var tmpVer = new Array();
+                this.#adjMatrix[i].forEach(function(element, index) {
+                    if (element !== 0) { tmpVer.push(index); }
+                });
+                this.#adjList[i] = tmpVer;
+                tmpVer = [];
+            }
+            console.log(this.#adjList);
         } else if (typeof arg === "object") {
             this.#adjList = arg;
+            this.#adjMatrix = new Array(Object.keys(this.#adjList).length).fill().map(() =>
+                new Array(Object.keys(this.#adjList).length.fill(0))); 
 
         } else {
             throw "lolxd"; 
@@ -30,5 +42,27 @@ class Graph {
 
 /** @return {void} */
 function main() {
+    var list_g_1 = {
+        0: [1, 2, 4],
+        1: [0, 2],
+        2: [0, 4],
+        3: [1, 4],
+        4: [0, 2, 3]
+    };
 
+    var mat_g_1 = [
+        [0, 1, 1, 0, 1], 
+        [1, 0, 0, 1, 0], 
+        [1, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 1], 
+        [1, 0, 1, 1, 0]
+    ];
+
+    var mat_g_2 = [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]
+    ];
+    
+    var g = new Graph(mat_g_1);
 } main();
